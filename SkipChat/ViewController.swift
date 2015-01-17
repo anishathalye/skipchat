06b496ac201bec37c472ad11aaceb06fe7662aa9
kathrynsiegel <ksiegel@mit.edu>
2015-01-17 00:25:50 -0500

    Initial Commit

---
+++ SkipChat/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SkipChat
+//
+//  Created by Katie Siegel on 1/17/15.
+//  Copyright (c) 2015 SkipChat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
