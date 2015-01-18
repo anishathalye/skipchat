4ac8190153f9f760a12f310518331ab5171253ff
Ankush Gupta <me@ankushg.com>
2015-01-17 20:01:47 -0500

    added delegate protocol and some implementation deets

--- Contagt/contagged.swift
+++ Contagt/contagged.swift
@@ -1,29 +1,56 @@
 //
-//  contagt.swift
+//  contagged.swift
 //  SkipChat
 //
 //  Created by Ankush Gupta on 1/17/15.
 //  Copyright (c) 2015 SkipChat. All rights reserved.
 //
-
-import Foundation
 import AddressBook
+import AddressBookUI
+import UIKit
 
-class ContaggedManager {
+
+
+class ContaggedManager: NSObject, ABPeoplePickerNavigationControllerDelegate {
     
-    init() {
-        let status : ABAuthorizationStatus = SwiftAddressBook.authorizationStatus()
-        if (status != ABAuthorizationStatus.Authorized){
-            swiftAddressBook?.requestAccessWithCompletion({ (success, error) -> Void in
-                if success {
-                    //do something with swiftAddressBook
-                } else {
-                    //no success, access denied. Optionally evaluate error
-                }
-            })
-        }
+    var delegate : ContaggedPickerDelegate?
+    
+    func getAuthorizationStatus() -> ABAuthorizationStatus {
+        return SwiftAddressBook.authorizationStatus()
     }
     
+    func requestAuthorizationWithCompletion(completion: (Bool, CFError?) -> Void ) {
+        swiftAddressBook?.requestAccessWithCompletion(completion)
+    }
+    
+    // MARK: Contact Picker Methods
+    func pickContact(fieldName: String) -> Void {
+        let picker = ABPeoplePickerNavigationController()
+        picker.peoplePickerDelegate = self
+        
+        if picker.respondsToSelector(Selector("predicateForEnablingPerson")) {
+            // urls.filter({$0.label == fieldName}).count > 0
+            picker.predicateForEnablingPerson = NSPredicate(format: "ANY urls.fieldName = %@", fieldName)
+        }
+        
+        //presentViewController(picker, animated: true, completion: nil)
+    }
+    
+    func peoplePickerNavigationController(peoplePicker: ABPeoplePickerNavigationController!,
+        didSelectPerson person: ABRecordRef!) {
+        delegate?.personSelected(swiftAddressBook?.personWithRecordId(ABRecordGetRecordID(person)))
+    }
+    
+    func peoplePickerNavigationController(peoplePicker: ABPeoplePickerNavigationController!, shouldContinueAfterSelectingPerson person: ABRecordRef!) -> Bool {
+        peoplePickerNavigationController(peoplePicker, didSelectPerson: person)
+        peoplePicker.dismissViewControllerAnimated(true, completion: nil)
+        return false;
+    }
+    
+    func peoplePickerNavigationControllerDidCancel(peoplePicker: ABPeoplePickerNavigationController!) {
+        peoplePicker.dismissViewControllerAnimated(true, completion: nil)
+        delegate?.peoplePickerNavigationControllerDidCancel();
+    }
     
     /**
     Add a field to a contact
@@ -43,6 +70,21 @@ class ContaggedManager {
         return swiftAddressBook?.save()
     }
     
+    /**
+    Find all contacts who have a given field
+    
+    :param: field The name of our custom field
+    
+    :returns: An Array of SwiftAddressBookPersons
+    */
+    func findContactsWithField(field:String) -> [SwiftAddressBookPerson]? {
+        // return all contacts who have at least one url with a matching label and value
+        return swiftAddressBook?.allPeople?.filter( {
+            $0.urls?.filter({
+                $0.label == field
+            }).count > 0
+        })
+    }
     
     /**
     Find all contacts who have a given value for a given field
@@ -52,7 +94,7 @@ class ContaggedManager {
     
     :returns: An Array of SwiftAddressBookPersons
     */
-    func findContactsByField(field:String, fieldValue:String) -> [SwiftAddressBookPerson]? {
+    func findContactsByFieldValue(field:String, fieldValue:String) -> [SwiftAddressBookPerson]? {
         // return all contacts who have at least one url with a matching label and value
         return swiftAddressBook?.allPeople?.filter( {
             $0.urls?.filter({
@@ -69,9 +111,13 @@ class ContaggedManager {
     
     :returns: An Array of SwiftAddressBookPersons
     */
-    func findMultivalueEntriesForPerson(field:String, record:ABRecord) -> [MultivalueEntry<String>]? {
+    func findValueForRecord(field:String, record:ABRecord) -> [MultivalueEntry<String>]? {
         let person = swiftAddressBook?.personWithRecordId(ABRecordGetRecordID(record))
         return person?.urls?.filter({$0.label == field})
     }
+}
 
+protocol ContaggedPickerDelegate  {
+    func peoplePickerNavigationControllerDidCancel()
+    func personSelected(person: SwiftAddressBookPerson!)
 }
