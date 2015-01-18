9b72ea4ae4dcd824f50e2438fdd66aa0bd8e1c18
Anish Athalye <me@anishathalye.com>
2015-01-17 21:37:35 -0500

    Crypto.m: Implement genKeyPair

--- SkipChat/Crypto.m
+++ SkipChat/Crypto.m
@@ -8,9 +8,14 @@
 
 #import "Crypto.h"
 #import <openssl/rand.h>
+#import <openssl/evp.h>
+#import <openssl/bio.h>
 #import <openssl/rsa.h>
+#import <openssl/pem.h>
 
 #define RNG_SEED_BYTES (20)
+#define RSA_BITS (4096)
+#define RSA_EXPONENT (65537) // see docs for recommendations, or http://en.wikipedia.org/wiki/Coppersmith%27s_Attack
 
 @interface Crypto ()
 
@@ -34,16 +39,51 @@
 + (KeyPair *) genKeyPair
 {
     [self maybeSeedRNG];
-    return nil;
+
+    KeyPair *kp = [[KeyPair alloc] init];
+
+    RSA *rsa = RSA_generate_key(RSA_BITS, RSA_EXPONENT, NULL, NULL);
+    BIO *bio;
+    int keyLength;
+
+    // serialize private key
+    bio = BIO_new(BIO_s_mem());
+    if (PEM_write_bio_RSAPrivateKey(bio, rsa, NULL, NULL, 0, NULL, NULL) != 1) {
+        NSLog(@"Failed to write RSA private key");
+        return nil;
+    }
+    keyLength = BIO_pending(bio);
+    NSMutableData *privateKey = [[NSMutableData alloc] initWithLength:(keyLength + 1)]; // reserve space for '\0'
+    BIO_read(bio, privateKey.mutableBytes, keyLength);
+    BIO_free_all(bio);
+    kp.privateKey = [NSData dataWithData:privateKey]; // immutable copy
+
+    // serialize public key
+    bio = BIO_new(BIO_s_mem());
+    if (PEM_write_bio_RSAPublicKey(bio, rsa) != 1) {
+        NSLog(@"Failed to write RSA public key");
+        return nil;
+    }
+    keyLength = BIO_pending(bio);
+    NSMutableData *publicKey = [[NSMutableData alloc] initWithLength:(keyLength + 1)]; // reserve space for '\0'
+    BIO_read(bio, publicKey.mutableBytes, keyLength);
+    BIO_free_all(bio);
+    kp.publicKey = [NSData dataWithData:publicKey]; // immutable copy
+
+    RSA_free(rsa);
+
+    return kp;
 }
 
 + (NSData *) sign:(NSData *) message with:(NSData *) privateKey andEncryptFor:(NSData *) publicKey
 {
+    [self maybeSeedRNG];
     return nil;
 }
 
 + (BOOL) decrypt:(NSData *) blob with:(NSData *)privateKey into:(NSData **) buffer from:(NSData **) publicKey
 {
+    [self maybeSeedRNG];
     return NO;
 }
 
